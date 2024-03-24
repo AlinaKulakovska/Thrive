cff330439046bb7e97420250402a36234d109ff4
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2024-03-24 20:58:19 +0200

    .

--- script.js
+++ script.js
@@ -53,8 +53,35 @@ var pecipeList = [{
     "Ingridiensts": "<li>Onion - 50g</li><li>Fish -250g</li><li>Lemon - 1/2</li>",
     "Nutrition": "150cal|15g|23g|21g",
     "Price": "$20"
+},
+{
+    "idMeal": "5",
+    "img": "https://www.allrecipes.com/thmb/GwdML2o-xsFGnHTEp-uy7RWMEOc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/166638-baked-buffalo-wings-ddmfs-beauty-3x4-90787930d20b402e9dde79c22fced332.jpg",
+    "rating": "4",
+    "reviews": "74",
+    "name": "Spicy Chicken Wings",
+    "time": "40 min",
+    "hardness": "easy",
+    "Category": "Non-Vegan",
+    "strInstructions": "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. ",
+    "Ingridiensts": "<li>Chicken Wings - 250g</li><li>oil - 500ms</li><li>egg</li>",
+    "Nutrition": "250cal|54g|23g|17g",
+    "Price": "$15"
 }
 ];
+
+
+const input = document.getElementById('Search');
+
+input.addEventListener("search", addEventListener("search", () => {
+    var newarray = []
+    for (let i = 0; i < pecipeList.length; i++) {
+        if (pecipeList[i].name.includes(input.value)) {
+            newarray.push(pecipeList[i])
+           
+        }}console.log(newarray);
+        codeAddress(newarray);
+})); 
 
 
 
@@ -68,13 +95,13 @@ function sortTime() {
             return -1;
         }
     });
-    codeAddress();
+    codeAddress(pecipeList);
 }
 
-
-function codeAddress() {
+function codeAddress(array) {
     document.getElementById("paginated-list").innerHTML = "";
-    for (let i = 0; i < pecipeList.length; i++) {
+
+    for (let i = 0; i < array.length; i++) {
 
         let meal = document.createElement("li");
         document.getElementById("paginated-list").appendChild(meal);
@@ -83,7 +110,7 @@ function codeAddress() {
         rating.classList.add("card_rating");
 
         for (let x = 0; x < 5; x++) {
-            if (x >= pecipeList[i].rating) {
+            if (x >= array[i].rating) {
                 let img = document.createElement("img");
                 img.style.height = "15px";
                 img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsmn6LPTD6BY0te9Qk_9jM_mOBcbVn8lae9w&usqp=CAU";
@@ -99,39 +126,39 @@ function codeAddress() {
         const card_list = document.createElement("div");
         card_list.insertAdjacentHTML("beforeend", `
     <a href="#" class="meal_link">
-        <img src="${pecipeList[i].img}" class="card_img">
+        <img src="${array[i].img}" class="card_img">
             <div class="card_rating">
 
                ${rating.innerHTML}
-                    ${pecipeList[i].reviews} Ratings
+                    ${array[i].reviews} Ratings
             </div>
         </img>
         <div class="card_body">
             <div class="card_name">
-                ${pecipeList[i].name}
+                ${array[i].name}
             </div>
             <div class="card_properties">
                 <div class="properties_circle"><img
                         src="https://cdn-icons-png.flaticon.com/512/826/826165.png" width="25px" alt="">
-                </div>${pecipeList[i].time}
+                </div>${array[i].time}
                 <div class="properties_circle"><img
                         src="https://cdn-icons-png.flaticon.com/512/7780/7780470.png" width="25px"
                         alt="">
-                </div>${pecipeList[i].hardness}
+                </div>${array[i].hardness}
                 <div class="properties_circle"><img
                         src="https://cdn-icons-png.flaticon.com/512/5627/5627100.png" width="25px"
                         alt="">
-                </div>${pecipeList[i].Category}
+                </div>${array[i].Category}
             </div>
 
-                <p class="card_description">${pecipeList[i].strInstructions}</p>
+                <p class="card_description">${array[i].strInstructions}</p>
                 <ul class="card_list">
-                ${pecipeList[i].Ingridiensts}
+                ${array[i].Ingridiensts}
                 </ul>
 
             <div class="card_nurtition_price">
-                <div class="nutrition">${pecipeList[i].Nutrition}</div>
-                <div class="price"><b>${pecipeList[i].Price}</b></div>
+                <div class="nutrition">${array[i].Nutrition}</div>
+                <div class="price"><b>${array[i].Price}</b></div>
             </div>
         </div>
     </a>`,
@@ -243,4 +270,4 @@ function pagination() {
     });
 
 }
-window.onload = codeAddress()
+window.onload = codeAddress(pecipeList)
